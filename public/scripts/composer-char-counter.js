7a62d3cd64606219219b9153206cbbe0b657f4ed
Andrew Bradt <andrewjbradt@gmail.com>
2022-03-01 10:11:02 -0500

    Created monitorCharCount closure which returns changeCounterStyle - changes counter color

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
+  const changeCounterStyle = monitorCharCount();
   $('.new-tweet textarea').on('input', function(){
     const remainingChars = _getRemainingChars(this.value);
     const counter = $(this).siblings().children().last();
     counter.text(remainingChars);
+    changeCounterStyle(counter, remainingChars);
   });
+  
 });
 
 const _getRemainingChars = (val) => {
@@ -11,3 +14,17 @@ const _getRemainingChars = (val) => {
   return tweetSize - val.length;
 };
 
+const monitorCharCount = () => {
+  let prevCharCount = 140;
+  const changeCounterStyle = (el, remainingChars) => {
+    const addClassCond = prevCharCount > 0 && remainingChars < 0;
+    const removeClassCond = prevCharCount < 0 && remainingChars > 0;
+    if (addClassCond) {
+      el.addClass('exceeded-char-count');
+    } else if (removeClassCond) {
+      el.removeClass('exceeded-char-count');
+    }
+    prevCharCount = remainingChars;
+  };
+  return changeCounterStyle;
+};
