494e14dd580e29f27895755f9650907c3a24535e
Andrew Bradt <andrewjbradt@gmail.com>
2022-03-01 21:04:14 -0500

    implemented renderTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,17 +3,30 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-};
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
 
 const createTweetElement = (data) => {
   const {user, content, created_at} = data;
@@ -28,7 +41,7 @@ const createTweetElement = (data) => {
     </header>
     <p>${content.text}</p>
     <footer>
-      <span>${created_at}</span>
+      <span>${timeago.format(created_at)}</span>
       <div>
         <i class="fa-solid fa-flag"></i>
         <i class="fa-solid fa-retweet"></i>
@@ -40,8 +53,9 @@ const createTweetElement = (data) => {
   return markup;
 };
 
+const renderTweets = (tweets) => tweets.map(tweet=>createTweetElement(tweet));
+
 $(()=>{
-  const $tweet = createTweetElement(tweetData);
-  $('main').append($tweet);
+  $('main').append(renderTweets(data));
 });
 // console.log($tweet);
