7ef331c61cea3762a3f196acdd8c63e4fc264456
Andrew Bradt <andrewjbradt@gmail.com>
2022-03-01 20:56:51 -0500

    Made createTweetElement function and append to DOM

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,45 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+};
 
+const createTweetElement = (data) => {
+  const {user, content, created_at} = data;
+  const markup = `
+  <article class="tweet">
+    <header>
+      <div>
+        <img src="${user.avatars}" alt="face">
+        <span>${user.name}</span>
+      </div>
+      <span>${user.handle}</span>
+    </header>
+    <p>${content.text}</p>
+    <footer>
+      <span>${created_at}</span>
+      <div>
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
+      </div>
+    </footer>
+  </article>
+  `;
+  return markup;
+};
+
+$(()=>{
+  const $tweet = createTweetElement(tweetData);
+  $('main').append($tweet);
+});
+// console.log($tweet);
